31036a0116ee9288c2ff689ed4e9135897e244fe
sonisanjana429 <sonisanjana429@gmail.com>
2026-02-20 17:50:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,29 @@
-#variable "vpc_name"{
-#  type = string
-#  default="testvpcone"
-#}
+variable "vpc_name"{
+  type = string
+  default="testvpcone"
+}
 
-#variable "vpc_tags" {
-#  type = list(string)
-#  default = ["tag:test1", "tag:test2"]
-#}
+variable "vpc_tags" {
+  type = list(string)
+  default = ["tag:test1", "tag:test2"]
+}
 
-#variable "api_key" {
-#  type = string
-#  default = ""
-#}
+variable "api_key" {
+  type = string
+  default = ""
+}
+
+variable "vpc_name_1"{
+  type = string
+  default="testvpcone"
+}
+
+variable "vpc_tags_1" {
+  type = list(string)
+  default = ["tag:test1", "tag:test2"]
+}
+
+variable "api_key_1" {
+  type = string
+  default = ""
+}
